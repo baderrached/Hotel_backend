8111eb8a2178731e1296b0b47b1696814b4d178c
iheb nachet <inachet3@gmail.Com>
2021-07-16 17:37:28 +0100

    final

--- routes/router.js
+++ routes/router.js
@@ -26,6 +26,9 @@ router.get('/extra',async(req,res)=>{
 })
 router.post('/CreateOrder',async(req,res)=>{
   console.log(req.body);
+  req.body.orders=JSON.stringify(req.body.orders)
+  const order=await axios.post(`http://localhost/api.php/records/orders`,req.body)
+  
   res.send(req.body)
 })
 // login 
@@ -64,7 +67,42 @@ router.post('/login', (req, res, next) => {
   
   });
 
+// register 
+router.post('/register', (req, res, next) => {
 
+
+  axios.get(`http://localhost/api.php/records/clients?filter=username,eq,${req.query.username}`).then(
+    (resp)=>{
+      if(resp.data.records.length>0){
+      return res.status(400).send({
+        msg: 'user name already used '
+      });
+      }else{
+          var object={
+          "username":req.query.username,
+          "password":req.query.password,
+          "passeport":req.query.passeport,
+
+          
+        }
+       
+        
+                  
+          axios.post(`http://localhost/api.php/records/clients`,object,head).then(
+
+            (resp)=>{
+              return res.status(200).send({
+              msg: 'Signup done'
+              }); 
+     
+            })
+
+          }
+          
+        }
+          
+          
+          )});
 
 
 
@@ -247,13 +285,14 @@ router.post('/login', (req, res, next) => {
 
 // update user
     router.post("/update/users/:id",(req,res)=>{
+   
       var id=req.params.id;
       var body=req.body
       axios.put(`http://localhost/api.php/records/clients/${id}`,body, head).then(
         (resp) => { 
-          axios.get(`http://localhost/api.php/records/user?filter=id,eq,${id}`).then(
+          axios.get(`http://localhost/api.php/records/clients?filter=id,eq,${id}`).then(
             (user)=>{
-              console.log(user);
+            
               res.send(user.data.records[0]);
             }
           )
@@ -268,47 +307,7 @@ router.post('/login', (req, res, next) => {
 
 
 
-  // register 
-  router.post('/signup', (req, res, next) => {
   
-
-    axios.get(`http://localhost/api.php/records/users?filter=username,eq,${req.body.username}`).then(
-      (resp)=>{
-        if(resp.data.records.length>0){
-        return res.status(400).send({
-          msg: 'user name already used '
-        });
-        }else{
-            var object={
-            "first_name":req.body.first_name,
-            "last_name":req.body.last_name,
-            "username":req.body.username,
-            "password":req.body.password,
-            "gender":req.body.gender,
-            "cin":req.body.cin,
-            "country":req.body.country,
-            "passeport":req.body.passeport,
-            "nb_reservation":req.body.nb_reservation
-
-            
-          }
-          
-                    
-            axios.post(`http://localhost/api.php/records/users`,object,head).then(
-
-              (resp)=>{
-                return res.status(401).send({
-                msg: 'Signup done'
-                }); 
-       
-              })
-
-            }
-            
-          }
-            
-            
-            )});
 
 const decrement_rooms=async(id,nb)=>{
 const room=await axios.get(`http://localhost/api.php/records/rooms/${id}`)
@@ -360,6 +359,66 @@ await axios.post('http://localhost/api.php/records/reservations',data)
 })
 
      }
+
+
+     router.post('/Reserve',async(req,res)=>{
+       //date2=new Date('2021-07-31T19:34:17.552Z').toLocaleDateString()
+       //date1=new Date("Sun Jul 18 2021 20:34:17 GMT+0100").toLocaleDateString()
+      // toLocaleDateString
+      //stime = stime.replace(/(..)\/(..)\/(.*)/, '$2/$1/$3');
+let date1=new Date(req.body.from).toLocaleDateString()
+let date2=new Date(req.body.to).toLocaleDateString()
+date1 = date1.replace(/(..)\/(..)\/(.*)/, '$2/$1/$3');
+
+date2 = date2.replace(/(..)\/(..)\/(.*)/, '$2/$1/$3');
+
+date1=new Date(date1)
+date2=new Date(date2)
+var Difference_In_Time = date2.getTime() - date1.getTime();
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+console.log({Difference_In_Days});
+
+console.log(req.body);
+      let Card={
+        "id_user":req.body.username,
+        "card_number":req.body.number,
+        "ex_date":req.body.exp,
+        "cvv":req.body.cvv
+      }
+      const pay=await axios.post(`http://localhost/api.php/records/payment`,Card)
+      
+      let reserver={
+        "user_id":req.body.username,
+        "room_id":req.body.date_room.id,
+        "from":new Date(req.body.from).toLocaleDateString(),
+        "to":new Date(req.body.to).toLocaleDateString(),
+        "amount":req.body.date_room.price*Difference_In_Days,
+        "status":0,
+        "nb_nuit":Difference_In_Days
+      }
+      console.log(reserver);
+      const ord= axios.post(`http://localhost/api.php/records/reservations`,reserver).catch(err=>{
+        console.log(err.message);
+      })
+
+       res.send('yes')
+     })
+     router.get('/findRoom/:id',async(req,res)=>{
+       let room=await axios.get(`http://localhost/api.php/records/reservations?filter=user_id,eq,${req.params.id}&order=id,desc`)
+       if(room.data.records.length==0){
+         return res.send({
+           "rooms":0,
+           "allrooms":[],
+           "last_room":[]
+         })
+       }
+       return res.send({
+        "rooms":room.data.records.length,
+        "allrooms":room.data.records,
+
+        "last_room":room.data.records[0]
+      })
+     })
 //check_availabe();
  //fake_reserve();
 module.exports = router;
